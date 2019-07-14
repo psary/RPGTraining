1a80cc1e587873f6a22f9fddcfe63a9882ea857c
Psary <4nthony.collin@gmail.com>
2019-07-14 14:39:22 +0200

    add home button

--- src/infra/view/common/Header.js
+++ src/infra/view/common/Header.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import GameAdder from '../component/header/GameAdder.js/index.js'
+import GameAdder from '../component/header/GameAdder.js'
 import './Header.css'
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
+import { withRouter } from 'react-router-dom'
 
 class Header extends Component {
+    goHome(){
+        this.props.history.push({pathname: '/'});
+    }
     render() {
         return (
-            <div className="Header"> RPG Training <GameAdder /></div>
+            <div className="Header"> 
+                <IconButton onClick={this.goHome.bind(this)}>
+                    <HomeIcon color="primary" />
+                </IconButton>
+                RPG Training 
+                <GameAdder />
+            </div>
         );
     }
 }
 
-export default Header;
+export default withRouter(Header);
